9448edcfa41574be497e44934beec50d49201f8e
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2020-06-23 18:09:15 +0100

    Basic template for electron app using Haxe

---
+++ App.hx
@@ -0,0 +1,27 @@
+import js.Browser.console;
+import js.Browser.document;
+import js.Browser.window;
+import js.Node.process;
+import js.node.ChildProcess;
+
+class App {
+
+    static inline function setText(id : String, text : String) {
+        document.getElementById(id).textContent = text;
+    }
+
+    static function main() {
+
+        window.onload = function() {
+            setText( 'system-version', process.platform + ' ' + process.arch );
+            setText( 'node-version', 'node ' + process.version );
+            setText( 'electron-version', 'electron ' + process.versions['electron'] );
+
+            ChildProcess.spawn( 'haxe', ['-version'] ).stdout.on( 'data', function(buf) {
+				var version = buf.toString();
+                setText( 'haxe-version', 'haxe $version' );
+            });
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # haxe-app
-An Electron App built using Haxe
+An Electron App built using the Haxe programming language.
+
+```sh
+cd haxe-app/
+
+## Install electron
+haxelib install electron
+
+## Build and run the app
+haxe build.hxml
+npm install
+npm run start
+```
+
